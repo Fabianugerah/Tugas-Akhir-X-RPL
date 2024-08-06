27aeb62af0b32b56a45eb075a80b601eed50cf84
Fabianugerah <fabianugerah@gmail.com>
2024-08-06 08:46:56 +0700

    Tugas Akhir Kelas X-RPL

---
+++ script.js
@@ -0,0 +1,79 @@
+// Opening Loading Screen
+window.addEventListener('load', function() {
+    setTimeout(function() {
+      document.querySelector('.loader-container').style.display = 'none';
+    }, 1250); 
+});
+
+// Sneakers
+function sneakers_1(){
+    document.getElementById("sneakers1").innerHTML = "Berhasil";
+}
+function sneakers_2(){
+    document.getElementById("sneakers2").innerHTML = "Berhasil";
+}
+function sneakers_3(){
+    document.getElementById("sneakers3").innerHTML = "Berhasil";
+}
+function sneakers_4(){
+    document.getElementById("sneakers4").innerHTML = "Berhasil";
+}
+function sneakers_5(){
+    document.getElementById("sneakers5").innerHTML = "Berhasil";
+}
+function sneakers_6(){
+    document.getElementById("sneakers6").innerHTML = "Berhasil";
+}
+function sneakers_7(){
+    document.getElementById("sneakers7").innerHTML = "Berhasil";
+}
+function sneakers_8(){
+    document.getElementById("sneakers8").innerHTML = "Berhasil";
+}
+
+// Running 
+function running_1(){
+    document.getElementById("running1").innerHTML = "Berhasil";
+}
+function running_2(){
+    document.getElementById("running2").innerHTML = "Berhasil";
+}
+function running_3(){
+    document.getElementById("running3").innerHTML = "Berhasil";
+}
+function running_4(){
+    document.getElementById("running4").innerHTML = "Berhasil";
+}
+function running_5(){
+    document.getElementById("running5").innerHTML = "Berhasil";
+}
+function running_6(){
+    document.getElementById("running6").innerHTML = "Berhasil";
+}
+function running_7(){
+    document.getElementById("running7").innerHTML = "Berhasil";
+}
+function running_8(){
+    document.getElementById("running8").innerHTML = "Berhasil";
+}
+function running_9(){
+    document.getElementById("running9").innerHTML = "Berhasil";
+}
+function running_10(){
+    document.getElementById("running10").innerHTML = "Berhasil";
+}
+function running_11(){
+    document.getElementById("running11").innerHTML = "Berhasil";
+}
+function running_12(){
+    document.getElementById("running12").innerHTML = "Berhasil";
+}
+function running_13(){
+    document.getElementById("running13").innerHTML = "Berhasil";
+}
+function running_14(){
+    document.getElementById("running14").innerHTML = "Berhasil";
+}
+function running_15(){
+    document.getElementById("running15").innerHTML = "Berhasil";
+}
