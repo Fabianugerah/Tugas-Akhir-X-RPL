9db828ea805b122dab4ad8cf6a28053b56f129bf
fabian nugrah <fabianugerah@gmail.com>
2026-04-13 20:48:02 +0700

    Change Language To English

--- script.js
+++ script.js
@@ -7,73 +7,73 @@ window.addEventListener('load', function() {
 
 // Sneakers
 function sneakers_1(){
-    document.getElementById("sneakers1").innerHTML = "Berhasil";
+    document.getElementById("sneakers1").innerHTML = "Success";
 }
 function sneakers_2(){
-    document.getElementById("sneakers2").innerHTML = "Berhasil";
+    document.getElementById("sneakers2").innerHTML = "Success";
 }
 function sneakers_3(){
-    document.getElementById("sneakers3").innerHTML = "Berhasil";
+    document.getElementById("sneakers3").innerHTML = "Success";
 }
 function sneakers_4(){
-    document.getElementById("sneakers4").innerHTML = "Berhasil";
+    document.getElementById("sneakers4").innerHTML = "Success";
 }
 function sneakers_5(){
-    document.getElementById("sneakers5").innerHTML = "Berhasil";
+    document.getElementById("sneakers5").innerHTML = "Success";
 }
 function sneakers_6(){
-    document.getElementById("sneakers6").innerHTML = "Berhasil";
+    document.getElementById("sneakers6").innerHTML = "Success";
 }
 function sneakers_7(){
-    document.getElementById("sneakers7").innerHTML = "Berhasil";
+    document.getElementById("sneakers7").innerHTML = "Success";
 }
 function sneakers_8(){
-    document.getElementById("sneakers8").innerHTML = "Berhasil";
+    document.getElementById("sneakers8").innerHTML = "Success";
 }
 
 // Running 
 function running_1(){
-    document.getElementById("running1").innerHTML = "Berhasil";
+    document.getElementById("running1").innerHTML = "Success";
 }
 function running_2(){
-    document.getElementById("running2").innerHTML = "Berhasil";
+    document.getElementById("running2").innerHTML = "Success";
 }
 function running_3(){
-    document.getElementById("running3").innerHTML = "Berhasil";
+    document.getElementById("running3").innerHTML = "Success";
 }
 function running_4(){
-    document.getElementById("running4").innerHTML = "Berhasil";
+    document.getElementById("running4").innerHTML = "Success";
 }
 function running_5(){
-    document.getElementById("running5").innerHTML = "Berhasil";
+    document.getElementById("running5").innerHTML = "Success";
 }
 function running_6(){
-    document.getElementById("running6").innerHTML = "Berhasil";
+    document.getElementById("running6").innerHTML = "Success";
 }
 function running_7(){
-    document.getElementById("running7").innerHTML = "Berhasil";
+    document.getElementById("running7").innerHTML = "Success";
 }
 function running_8(){
-    document.getElementById("running8").innerHTML = "Berhasil";
+    document.getElementById("running8").innerHTML = "Success";
 }
 function running_9(){
-    document.getElementById("running9").innerHTML = "Berhasil";
+    document.getElementById("running9").innerHTML = "Success";
 }
 function running_10(){
-    document.getElementById("running10").innerHTML = "Berhasil";
+    document.getElementById("running10").innerHTML = "Success";
 }
 function running_11(){
-    document.getElementById("running11").innerHTML = "Berhasil";
+    document.getElementById("running11").innerHTML = "Success";
 }
 function running_12(){
-    document.getElementById("running12").innerHTML = "Berhasil";
+    document.getElementById("running12").innerHTML = "Success";
 }
 function running_13(){
-    document.getElementById("running13").innerHTML = "Berhasil";
+    document.getElementById("running13").innerHTML = "Success";
 }
 function running_14(){
-    document.getElementById("running14").innerHTML = "Berhasil";
+    document.getElementById("running14").innerHTML = "Success";
 }
 function running_15(){
-    document.getElementById("running15").innerHTML = "Berhasil";
+    document.getElementById("running15").innerHTML = "Success";
 }
